aa65414fd3a92e785a5804afc8aa19ec203b6620
AyandaM <27950608+mabuselaaya@users.noreply.github.com>
2021-03-07 20:29:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,103 +1,202 @@
-<!DOCTYPE html >
-<html >
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Aodevsolutions - App</title>
 
-<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+    <link rel="stylesheet" href="css/all.min.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/templatemo-style.css" />
 
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    
-    <title>DynamicDNA Chat</title>
-    
-    <link rel="stylesheet" href="style.css" type="text/css" />
-    
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-    <script type="text/javascript" src="chat.js"></script>
-    <script type="text/javascript">
-    
+  </head>
+  <body>
+    <div class="parallax-window" data-parallax="scroll" data-image-src="img/bg-01.jpg">
+      <div class="container-fluid">
+        <div class="row tm-brand-row">
+          <div class="col-lg-4 col-11">
+            <div class="tm-brand-container tm-bg-white-transparent">
+              <div class="tm-brand-texts">
+                <h1 class="text-uppercase tm-brand-name">Aodevsolutions</h1>
+                <p class="small">AODS - Application</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8 col-1">
+            <div class="tm-nav">
+              <nav class="navbar navbar-expand-lg navbar-light tm-bg-white-transparent tm-navbar">
+                <button class="navbar-toggler" type="button"
+                  data-toggle="collapse" data-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <div class="tm-nav-link-highlight"></div>
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <div class="tm-nav-link-highlight"></div>
+                      <a class="nav-link" href="about.html">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <div class="tm-nav-link-highlight"></div>
+                      <a class="nav-link" href="services.html">Services</a>
+                    </li>
+                    <li class="nav-item">
+                      <div class="tm-nav-link-highlight"></div>
+                      <a class="nav-link" href="testimonials.html">Testimonials</a>
+                    </li>
+                    <li class="nav-item">
+                      <div class="tm-nav-link-highlight"></div>
+                      <a class="nav-link" href="contact.html">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        <section class="row" id="tmHome">
+          <div class="col-12 tm-home-container">
+            <div class="text-white tm-home-left">
+              <p class="text-uppercase tm-slogan">We can develop</p>
+              <hr class="tm-home-hr" />
+              <h2 class="tm-home-title">Multi-Purpose App for Your Business</h2>
+              <p class="tm-home-text">
+              </p>
+              <a href="#tmFeatures" class="btn btn-primary">Learn More</a>
+            </div>
+            <div class="tm-home-right">
+              <img src="img/mobile-screen.png" alt="App on Mobile mockup" />
+            </div>
+          </div>
+        </section>
+
+        <!-- Features -->
+        <div class="row" id="tmFeatures">
+          <div class="col-lg-4">
+            <div class="tm-bg-white-transparent tm-feature-box">
+            <h3 class="tm-feature-name">Database</h3>
             
-        var name = prompt("Enter your chat name:", "User");
+            <div class="tm-feature-icon-container">
+                <i class="fas fa-3x fa-server"></i>
+            </div>
+
+            <p class="text-center">MySQL Database Service is a fully managed database service to deploy cloud-native applications.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="tm-bg-white-transparent tm-feature-box">
+                <h3 class="tm-feature-name">Fast Support</h3>
+
+                <div class="tm-feature-icon-container">
+                  <i class="fa fa-cog fa-spin fa-3x fa-fw" aria-hidden="true"></i>
+<span class="sr-only"></span>
+                  <!--  <i class="fas fa-3x fa-browser"></i>-->
+                </div>
+                <p class="text-center">SDLC - Software Development Life Cycle.
+                </p>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="tm-bg-white-transparent tm-feature-box">
+                <h3 class="tm-feature-name">SEO</h3>
+
+                <div class="tm-feature-icon-container">
+                    <i class="fas fa-3x fa-satellite-dish"></i>
+                </div>
+                <p class="text-center">Search Engine Optimization.
+                </p>
+            </div>
+          </div>
+        </div>
         
-        
-    	if (!name || name === ' ') {
-    	   name = "User";	
-    	}
-    	
-    	
-    	name = name.replace(/(<([^>]+)>)/ig,"");
-    	
-    	
-    	$("#name-area").html("You are: <span>" + name + "</span>");
-    	
-    	
-        var chat =  new Chat();
-    	$(function() {
-    	
-    		 chat.getState(); 
-    		 
-    		 
-             $("#sendie").keydown(function(event) {  
-             
-                 var key = event.which;  
-           
-                  
-                 if (key >= 33) {
-                   
-                     var maxLength = $(this).attr("maxlength");  
-                     var length = this.value.length;  
-                     
-                     
-                     if (length >= maxLength) {  
-                         event.preventDefault();  
-                     }  
-                  }  
-    		 																																																});
-    		 
-    		 $('#sendie').keyup(function(e) {	
-    		 					 
-    			  if (e.keyCode == 13) { 
-    			  
-                    var text = $(this).val();
-    				var maxLength = $(this).attr("maxlength");  
-                    var length = text.length; 
-                     
-               
-                    if (length <= maxLength + 1) { 
-                     
-    			        chat.send(text, name);	
-    			        $(this).val("");
-    			        
-                    } else {
+
+        <!-- Features2 -->
+        <div class="row" id="tmFeatures">
+          <div class="col-lg-4">
+            <div class="tm-bg-white-transparent tm-feature-box">
+            <h3 class="tm-feature-name">App Development</h3>
+            
+            <div class="tm-feature-icon-container">
+                <i class="fas fa-3x fa-code"></i>
+            </div>
+
+            <p class="text-center">We develop custom software 
+              applications for web, desktop and mobile platforms.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="tm-bg-white-transparent tm-feature-box">
+                <h3 class="tm-feature-name">Graphic Design</h3>
+
+                <div class="tm-feature-icon-container">
+                 <!-- <i class="fa fa-cog fa-spin fa-3x fa-fw" aria-hidden="true"></i>
+<span class="sr-only"></span>
+<i class="fas fa-3x fa-sticky-note-o"></i>
+-->
                     
-    					$(this).val(text.substring(0, maxLength));
-    					
-    				}	
-    				
-    				
-    			  }
-             });
-            
-    	});
-    </script>
+                    <i class="fa-sticky-note-o" aria-hidden="true"></i>
+                </div>
+                <p class="text-center">Visual representations to enhance or 
+                  enable the representation of an idea.
 
-</head>
+                </p>
+            </div>
+          </div>
 
-<body onload="setInterval('chat.update()', 1000)">
+          <div class="col-lg-4">
+            <div class="tm-bg-white-transparent tm-feature-box">
+                <h3 class="tm-feature-name">Networking</h3>
 
-    <div id="page-wrap">
-        
-     <img src="Capture.jpg" width="30%" height="30%" align="left">
-        <h2>DynamicDNA' Level 4 Chat Site</h2>
-        
-        <p id="name-area"></p>
-        
-        <div id="chat-wrap"><div id="chat-area"></div></div>
-        
-        <form id="send-message-area">
-            <p>Your message: </p>
-            <textarea id="sendie" maxlength = '100' ></textarea>
-        </form>
-   
+                <div class="tm-feature-icon-container">
+                    <i class="fas fa-3x fa-satellite-dish"></i>
+                </div>
+                <p class="text-center">Network solutions.
+                </p>
+            </div>
+          </div>
+        </div>
+        <!-- Call to Action -->
+        <section class="row" id="tmCallToAction">
+          <div class="col-12 tm-call-to-action-col">
+            <img src="img/call-to-action.jpg" alt="Image" class="img-fluid tm-call-to-action-image" />
+            <div class="tm-bg-white tm-call-to-action-text">
+              <h2 class="tm-call-to-action-title">Updates</h2>
+              <p class="tm-call-to-action-description">
+                You may browse <a rel="nofollow" href="http://www.aodevsolutions.somee.com">AODS</a> on more  frequently
+                 asked questions (FAQ) forum is often used in articles, websites, email lists, and online forums where common questions tend to recur.
+              </p>
+              <form action="#" method="get" class="tm-call-to-action-form">                
+                <input name="email" type="email" class="tm-email-input" id="email" placeholder="Email" />
+                <button type="submit" class="btn btn-secondary">Get Updates</button>
+              </form>
+            </div>
+          </div>
+        </section>
+
+
+        <!-- Page footer -->
+        <footer class="row">
+          <p class="col-12 text-white text-center tm-copyright-text">
+            Copyright &copy; 2021 Aodevsolutions App. 
+            Designed by <a href="https://aodevsolutions.wordpress.com/" class="tm-copyright-link">AODS</a>
+          </p>
+        </footer>
+      </div>
+      <!-- .container-fluid -->
     </div>
 
-</body>
-<a href="get.php" target="_blank"><h1>History</h1></a>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
